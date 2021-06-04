ffade71a29c671458879b4559391ecd7aecdc4e6
Antonio Majic <antonio.majic@live.com>
2021-06-04 16:43:53 -0400

    Read and write to PPU.

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,10 +1,10 @@
-#include <cstdint>
-
 #include "bus.h"
 
 Bus::Bus(Cartridge& cart)
 	: cartridge(cart)
 	, ram(2048)
+	, vram(2048)
+	, palette_ram(32)
 {
 }
 
@@ -23,4 +23,48 @@ void Bus::CPUWrite(uint16_t address, uint8_t value)
 		ram[address & 0x07FF] = value;
 	else if (address >= 0x8000 && address <= 0xFFFF)
 		cartridge.CPUWrite(address, value);
+}
+
+uint8_t Bus::PPURead(uint16_t address)
+{
+	if (address >= 0x0000 && address <= 0x1FFF)
+		return cartridge.PPURead(address);
+	else if (address >= 0x2000 && address <= 0x3EFF) {
+		if (cartridge.GetMapper()->GetMirror()) {
+			// vertical (horizontal arrangement)
+			return vram[address & 0x07FF];
+		}
+		else {
+			// horizontal (vertical arrangement)
+			address &= 0x0FFF;
+			if (address <= 0x07FF)
+				return vram[address & 0x03FF];
+			else
+				return vram[(address & 0x03FF) + 0x0400];
+		}
+	}
+	else if (address >= 0x3F00 && address <= 0x3FFF)
+		return palette_ram[address & 0x001F];
+}
+
+void Bus::PPUWrite(uint16_t address, uint8_t value)
+{
+	if (address >= 0x0000 && address <= 0x1FFF)
+		cartridge.PPUWrite(address, value);
+	else if (address >= 0x2000 && address <= 0x3EFF) {
+		if (cartridge.GetMapper()->GetMirror()) {
+			// vertical (horizontal arrangement)
+			vram[address & 0x07FF] = value;
+		}
+		else {
+			// horizontal (vertical arrangement)
+			address &= 0x0FFF;
+			if (address <= 0x07FF)
+				vram[address & 0x03FF] = value;
+			else
+				vram[(address & 0x03FF) + 0x0400] = value;
+		}
+	}
+	else if (address >= 0x3F00 && address <= 0x3FFF)
+		palette_ram[address & 0x001F] = value;
 }
--- src/bus.h
+++ src/bus.h
@@ -13,7 +13,13 @@ public:
 	uint8_t CPURead(uint16_t address);
 	void CPUWrite(uint16_t address, uint8_t value);
 
+	uint8_t PPURead(uint16_t address);
+	void PPUWrite(uint16_t address, uint8_t value);
+
 private:
 	Cartridge& cartridge;
 	std::vector<uint8_t> ram;
+
+	std::vector<uint8_t> vram;
+	std::vector<uint8_t> palette_ram;
 };
