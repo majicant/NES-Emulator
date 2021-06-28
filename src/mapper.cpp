5d4dce85ded6ad33517e10d0db273e0b5df1137f
Antonio Majic <antonio.majic@live.com>
2021-06-28 00:09:12 -0400

    Added functions and other small changes to the base Mapper class.

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -3,6 +3,6 @@
 Mapper::Mapper(uint8_t prg_banks, uint8_t chr_banks, uint8_t mirror)
 	: prg_banks(prg_banks)
 	, chr_banks(chr_banks)
-	, mirror(mirror)
+	, mirror(mirror ? Mirror::VERTICAL : Mirror::HORIZONTAL)
 {
 }
--- src/mapper.h
+++ src/mapper.h
@@ -2,19 +2,34 @@
 
 #include <cstdint>
 
+enum class Mirror
+{
+	ONE_SCREEN_LO,
+	ONE_SCREEN_HI,
+	VERTICAL,
+	HORIZONTAL
+};
+
 class Mapper
 {
 public:
 	Mapper(uint8_t prg_banks, uint8_t chr_banks, uint8_t mirror);
 	virtual ~Mapper() = default;
 
-	virtual uint32_t MapCPURead(uint16_t address) = 0;
-	virtual void MapCPUWrite(uint16_t address, uint8_t value) = 0;
+	virtual uint32_t MapCPURead(uint16_t address) { return address; }
+	virtual void MapCPUWrite(uint16_t address, uint8_t value) {};
 
-	inline virtual uint8_t GetMirror() { return mirror; }
+	virtual uint32_t MapPPURead(uint16_t address) { return address; }
+	virtual void MapPPUWrite(uint16_t address, uint8_t value) {}
+
+	virtual uint8_t ReadPRGRAM(uint16_t address) { return 0x00; }
+	virtual void WritePRGRAM(uint16_t address, uint8_t value) {}
+
+	inline virtual Mirror GetMirror() { return mirror; }
+	inline virtual bool HasCHRRam() { return chr_banks == 0; }
 
 protected:
 	uint8_t prg_banks;
 	uint8_t chr_banks;
-	uint8_t mirror;
+	Mirror mirror;
 };
