83c13c4f6116d8437f93b216e3fe564f67646032
Antonio Majic <antonio.majic@live.com>
2021-06-28 00:06:24 -0400

    Added more mirroring and allowed for reading PRG RAM.

--- src/bus.cpp
+++ src/bus.cpp
@@ -19,7 +19,7 @@ uint8_t Bus::CPURead(uint16_t address)
 		return bus_ppu->Read(address & 0x0007);
 	else if (address == 0x4016)
 		return controller.Read();
-	else if (address >= 0x8000 && address <= 0xFFFF)
+	else if (address >= 0x6000 && address <= 0xFFFF)
 		return cartridge.CPURead(address);
 	return 0x00;
 }
@@ -34,7 +34,7 @@ void Bus::CPUWrite(uint16_t address, uint8_t value)
 		bus_cpu->TriggerOAMDMA(value);
 	else if (address == 0x4016)
 		controller.Write();
-	else if (address >= 0x8000 && address <= 0xFFFF)
+	else if (address >= 0x6000 && address <= 0xFFFF)
 		cartridge.CPUWrite(address, value);
 }
 
@@ -43,12 +43,15 @@ uint8_t Bus::PPURead(uint16_t address)
 	if (address >= 0x0000 && address <= 0x1FFF)
 		return cartridge.PPURead(address);
 	else if (address >= 0x2000 && address <= 0x3EFF) {
-		if (cartridge.GetMapper()->GetMirror()) {
-			// vertical (horizontal arrangement)
+		switch (cartridge.GetMapper()->GetMirror()) {
+		case Mirror::ONE_SCREEN_LO:
+			return vram[address & 0x03FF];
+		case Mirror::ONE_SCREEN_HI:
+			address &= 0x07FF;
+			return vram[(address <= 0x3FFF) ? (address + 0x0400) : address];
+		case Mirror::VERTICAL:
 			return vram[address & 0x07FF];
-		}
-		else {
-			// horizontal (vertical arrangement)
+		case Mirror::HORIZONTAL:
 			address &= 0x0FFF;
 			return vram[(address <= 0x07FF) ? (address & 0x03FF) : ((address & 0x03FF) + 0x0400)];
 		}
@@ -71,14 +74,21 @@ void Bus::PPUWrite(uint16_t address, uint8_t value)
 	if (address >= 0x0000 && address <= 0x1FFF)
 		cartridge.PPUWrite(address, value);
 	else if (address >= 0x2000 && address <= 0x3EFF) {
-		if (cartridge.GetMapper()->GetMirror()) {
-			// vertical (horizontal arrangement)
+		switch (cartridge.GetMapper()->GetMirror()) {
+		case Mirror::ONE_SCREEN_LO:
+			vram[address & 0x03FF] = value;
+			break;
+		case Mirror::ONE_SCREEN_HI:
+			address &= 0x07FF;
+			vram[(address <= 0x3FFF) ? (address + 0x0400) : address] = value;
+			break;
+		case Mirror::VERTICAL:
 			vram[address & 0x07FF] = value;
-		}
-		else {
-			// horizontal (vertical arrangement)
+			break;
+		case Mirror::HORIZONTAL:
 			address &= 0x0FFF;
 			vram[(address <= 0x07FF) ? (address & 0x03FF) : ((address & 0x03FF) + 0x0400)] = value;
+			break;
 		}
 	}
 	else if (address >= 0x3F00 && address <= 0x3FFF) {
