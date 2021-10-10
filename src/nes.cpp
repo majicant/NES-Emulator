9e699e728b3e0c41ac3c0dce312ba036afff4622
Antonio Majic <antonio.majic@live.com>
2021-10-09 22:44:22 -0400

    Forgot to clock PPU at three times the CPU clock.

--- src/nes.cpp
+++ src/nes.cpp
@@ -1,6 +1,6 @@
 #include "nes.h"
 
-NES::NES(const std::string& filename)
+NES::NES(const std::filesystem::path& filename)
 	: nes_cartridge(std::make_unique<Cartridge>(filename))
 	, nes_engine(std::make_unique<SDLEngine>())
 	, nes_controller(std::make_unique<Controller>(*nes_engine))
@@ -17,7 +17,7 @@ void NES::Run()
 	unsigned ppu_cycles;
 	while (!nes_engine->GetExitFlag()) {
 		ppu_cycles = nes_ppu->CheckNMI() ? nes_cpu->HandleNMI() : nes_cpu->CheckOAMDMA() ? nes_cpu->HandleOAMDMA() : nes_cpu->ExecuteInstruction();
-		for (unsigned i = 0; i < ppu_cycles; i++)
+		for (unsigned i = 0; i < ppu_cycles * 3; i++)
 			nes_ppu->Step();
 	}
 }
